10e0af8c3c862bb52edf456b27327a61e4d614a7
mckinsta <morekoushal@gmail.com>
2026-05-03 18:11:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from pyrogram import Client
 from config import BOT_TOKEN, API_ID, API_HASH
+import threading
+from http.server import HTTPServer, BaseHTTPRequestHandler
 
 app = Client(
     "movie_bot",
@@ -7,5 +9,18 @@ app = Client(
     api_id=API_ID,
     api_hash=API_HASH
 )
+
+# 🔥 Dummy server for Koyeb
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Bot is running")
+
+def run_server():
+    server = HTTPServer(("0.0.0.0", 8000), Handler)
+    server.serve_forever()
+
+threading.Thread(target=run_server).start()
 
 app.run()
