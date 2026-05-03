9c1993a8aeaa9e1191caa03c98f89f7350488640
mckinsta <morekoushal@gmail.com>
2026-05-03 17:45:52 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+BOT_TOKEN = "8743936811:AAF9VWrkq8zMURZtfdpvBLa-i2GiKk9dHN0"
+API_ID = 22083216
+API_HASH = "a4c3ce9493061323cbb5741370aa11f1"
+
+ADMINS = [1489423238]
+
+DATABASE_URI = "mongodb+srv://Koushal:Koushal@koushal.zjd00.mongodb.net/?appName=Koushal"
+
+LOG_CHANNEL = -1003913030328
+FORCE_SUB = None  # optional
+
+UPI_ID = "morekoushal@oksbi"
+OWNER = "@morekoushal1444"
+
+LOGO = "https://your-logo-link.jpg"
+QR = "https://your-qr-link.jpg"
